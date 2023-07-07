8c23cefbd89348b8ef524c877e90f0a842129fb6
nidhi2793 <MY_NAME@example.com>
2023-07-07 13:39:36 +0530

    first

---
+++ script.js
@@ -0,0 +1,85 @@
+console.log("tic tac toe");
+let music = new Audio("music.mp3");
+let turnMusic = new Audio("ting.mp3");
+let gameoverMusic = new Audio("gameover.mp3");
+let gameOver = false;
+
+let turn = "X";
+
+const changeTurn = () => {
+  if (turn === "X") {
+    turn = "0";
+  } else {
+    turn = "X";
+  }
+};
+
+const checkWin = () => {
+  let boxtext = document.getElementsByClassName("boxText");
+  let wins = [
+    [0, 1, 2, 5, 5, 0],
+    [3, 4, 5, 5, 15, 0],
+    [6, 7, 8, 5, 25, 0],
+    [0, 3, 6, -5, 15, 90],
+    [1, 4, 7, 5, 15, 90],
+    [2, 5, 8, 15, 15, 90],
+    [0, 4, 8, 5, 15, 45],
+    [2, 4, 6, 5, 15, 135],
+  ];
+
+  wins.forEach((e) => {
+    if (
+      boxtext[e[0]].innerText === boxtext[e[1]].innerText &&
+      boxtext[e[2]].innerText === boxtext[e[1]].innerText &&
+      boxtext[e[0]].innerText !== ""
+    ) {
+      gameOver = true;
+      gameoverMusic.play();
+      document.querySelector(".info").innerText =
+        boxtext[e[0]].innerText + " Won!!";
+
+      document
+        .querySelector(".imageBox")
+        .getElementsByTagName("img")[0].style.width = "150px";
+
+      document.querySelector(
+        ".line"
+      ).style.transform = `translate(${e[3]}vw,${e[4]}vw) rotate(${e[5]}deg)`;
+      document.querySelector(".line").style.width = "20vw";
+    }
+  });
+};
+// music.play();
+let boxes = document.getElementsByClassName("box");
+Array.from(boxes).forEach((element) => {
+  let boxtext = element.querySelector(".boxText");
+  element.addEventListener("click", () => {
+    if (boxtext.innerText === "") {
+      boxtext.innerText = turn;
+      changeTurn();
+      turnMusic.play();
+      checkWin();
+
+      if (gameOver == false) {
+        document.getElementsByClassName("info")[0].innerText =
+          "Turn for " + turn;
+      }
+    }
+  });
+});
+
+const resetGame = () => {
+  let boxtexts = document.querySelectorAll(".boxText");
+  Array.from(boxtexts).forEach((element) => {
+    element.innerText = "";
+  });
+  gameOver = false;
+  turn = "X";
+  document.getElementsByClassName("info")[0].innerText = "Turn for " + turn;
+  document
+    .querySelector(".imageBox")
+    .getElementsByTagName("img")[0].style.width = "0";
+  document.querySelector(".line").style.width = "0vw";
+};
+
+reset.addEventListener("click", resetGame);
